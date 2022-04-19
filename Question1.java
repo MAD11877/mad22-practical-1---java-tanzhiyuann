79b82235676ffda91974daeea7761cf8ffecec60
Tan Zhi Yuan <tanyuan2002@gmailcom>
2022-04-19 22:47:56 +0800

    Q1 test 3

--- Question1.java
+++ Question1.java
@@ -1,9 +1,6 @@
 public class Question1
 {
-  public static void main(String[] args)
-  {
-
-    System.out.println("Hello World!");
-   
-  }
+  public static void main(String args[]) { 
+    System.out.println("Hello world!"); 
+  } 
 }
