3e6e2ef74d0c3544ec7b6d4263d2e5c9dfd33fb1
Tan Zhi Yuan <tanyuan2002@gmailcom>
2022-04-20 12:18:08 +0800

    q2

--- Question2.java
+++ Question2.java
@@ -12,5 +12,12 @@ public class Question2
      
     Scanner in = new Scanner(System.in);
     double height = in.nextDouble();
+
+
+    double weight = in.nextDouble();
+
+    double bmi = weight / height*height;
+
+    System.out.printIn(bmi);
   }
 }
